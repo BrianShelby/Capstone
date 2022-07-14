7bac3b74f132cba42691a6f6d8ffb009310f227b
Brian Shelby <bsshel@yahoo.com>
2022-07-14 15:59:09 -0500

    Update Capstone SPA

---
+++ components/views/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Petition } from "./Petition";
+export { default as Register } from "./Register";
+export { default as Mail } from "./Mail";
