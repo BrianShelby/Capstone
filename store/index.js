e0f20642aa3b5ad0baf0b7ec1c7a85191ac89f18
Brian Shelby <bsshel@yahoo.com>
2022-07-14 21:34:48 -0500

    Update SPA files

---
+++ store/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as Petition } from "./Petition";
+export { default as Register } from "./Register";
+export { default as Mail } from "./Mail";
+export { default as Links } from "./Links";
