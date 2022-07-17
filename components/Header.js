65c246f647be9cffb2b5aa86e27d70dd4427a35b
Brian Shelby <bsshel@yahoo.com>
2022-07-17 16:09:22 -0500

    Update Capstone

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,25 @@
 import html from "html-literal";
+import * as links from "../store/Links";
 
 export default state => html`
   <header>
     <h1>${state.header}</h1>
+    <a href="#"><img id="logo" src="Pictures/logo.png"/></a>
+    <video autoplay loop muted plays-inline class="back-video">
+      <source src="Pictures/usaFlag.mp4" type="video/mp4" />
+    </video>
+    <nav>
+      <ul class="nav-links">
+        ${links
+          .map(
+            links =>
+              `<li><a href="/${links.title}" title="${links.title}" data-navigo>${links.text}</a></li>`
+          )
+          .join("")}
+      </ul>
+      <div class="hamburger">
+        <i class="fas fa-bars"></i>
+      </div>
+    </nav>
   </header>
 `;
