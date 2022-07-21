7977cc965ee32973814750d386f837f5ff1c57f9
Brian Shelby <bsshel@yahoo.com>
2022-07-21 15:01:29 -0500

    Capstone Update

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -6,16 +6,12 @@ export default () => html`
     <h1>Brian Shelby Bio</h1>
     <div><img class="bioPic" src="${Bio1}" alt="" /></div>
     <h2>
-      Each initiative and referendum state employs a different procedure for
-      filing petition applications. Some states require preliminary signatures
-      while others do not. In addition, several states review each proposed
-      statute, verifying that the law conforms to the style and conventions of
-      state law and recommending alterations to initiative proponents.<a
-        href="https://www.sos.mo.gov/CMSImages/Elections/Petitions/MakeYourVoiceHeard2022Cycle.pdf"
-        target="_blank"
-      >
-        Click here to see The Missouri Official Handbook.</a
-      >
+      Hi! My name is Brian Shelby. I have a background in manufacturing, where
+      over the past 25 years I have held various roles ranging from lead
+      assembly mechanic, shop steward, and operations manager. In 2017, I
+      obtained my BS degree in Information Technology. My love for technology,
+      education and problem solving has led me to Savvy Coders Full Stack
+      Bootcamp.
     </h2>
   </section>
 `;
