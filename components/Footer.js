f133ca0dab60fd412807fa196cd682ddea6732a2
Brian Shelby <bsshel@yahoo.com>
2022-07-22 16:00:27 -0500

    Add Capstone

--- components/Footer.js
+++ components/Footer.js
@@ -3,5 +3,5 @@ import html from "html-literal";
 let year = new Date().getFullYear();
 
 export default () => html`
-  <footer>&copy;${year} <a href="index.html">YouVote</a></footer>
+  <footer>&copy;${year} <a href="/">YouVote</a></footer>
 `;
