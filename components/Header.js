084577e62a1b35fc99965f1f23dac169d7e87f6f
Brian Shelby <bsshel@yahoo.com>
2022-07-21 21:08:42 -0500

    Capstone Update

--- components/Header.js
+++ components/Header.js
@@ -10,12 +10,14 @@ export default state => html`
       <div class="btn">
         <i class="fas fa-times close-btn"></i>
       </div>
-      ${links
-        .map(
-          link =>
-            `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
-        )
-        .join("")}
+      <ul>
+        ${links
+          .map(
+            link =>
+              `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
+          )
+          .join("")}
+      </ul>
     </div>
     <div class="btn">
       <i class="fas fa-bars menu-btn"></i>
