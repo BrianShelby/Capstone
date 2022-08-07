5cd8e96be580fb61e1c785976a0a370a3c8d2af4
Brian Shelby <bsshel@yahoo.com>
2022-08-06 23:25:07 -0500

    Update Cap

--- components/views/Search.js
+++ components/views/Search.js
@@ -2,23 +2,40 @@ import html from "html-literal";
 
 export default st => html`
   <section class="section-main">
-    <h1>Area Legislators</h1>
-    <table>
-      <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Office Name</th>
-        <th>Election Office</th>
-        <th>Election Status</th>
-      </tr>
-      ${st.legislators.map(legislator => {
-        return `<tr><td>
-          ${legislator.firstName}</td><td>
-          ${legislator.lastName}</td><td>
-          ${legislator.officeName}</td><td>
-          ${legislator.electionOffice}</td><td>
-          ${legislator.electionStatus}</td></tr>`;
-      })}
-    </table>
+    <h1>State & Federal Legislators</h1>
+
+    <div class="candidate-container1">
+      <div class="candidate-heading1">First Name</div>
+      <div class="candidate-heading2">Last Name</div>
+      <div class="candidate-heading3">Election Office</div>
+    </div>
+    ${st.legislators.map(legislator => {
+      return `
+          <div class="candidate-container2">
+          <div class="candidate-content1">${legislator.firstName}</div>
+          <div class="candidate-content2">${legislator.lastName}</div>
+          <div class="candidate-content3">${legislator.electionOffice}</div>
+          </div>`;
+    })}
   </section>
 `;
+
+{
+  /* <table>
+<tr>
+  <th>First Name</th>
+  <th>Last Name</th>
+  <th>Office Name</th>
+  <th>Election Office</th>
+  <th>Election Status</th>
+</tr>
+${st.legislators.map(legislator => {
+  return `<tr><td>
+    ${legislator.firstName}</td><td>
+    ${legislator.lastName}</td><td>
+    ${legislator.officeName}</td><td>
+    ${legislator.electionOffice}</td><td>
+    ${legislator.electionStatus}</td></tr>`;
+})}
+</table> */
+}
